2e50017cbdc110181b9cd56adc49848853661c30
Anupam <anupamsingh2389@gmail.com>
2024-09-10 16:53:19 +0530

    Refactor Todo component styles for better UI and UX

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -43,13 +43,13 @@ const Todo = () => {
   }, [todoList]);
 
   return (
-    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl overflow-hidden">
       <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={todo_icon} alt="" />
         <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
 
-      <div className="flex item-center my-7 bg-gray-200 rounded-full">
+      <div className="flex items-center my-7 bg-gray-200 rounded-full max-w-full overflow-hidden">
         <input
           ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
@@ -63,7 +63,8 @@ const Todo = () => {
           ADD+
         </button>
       </div>
-      <div>
+
+      <div className="overflow-y-auto max-h-[300px]">
         {todoList.map((item, index) => {
           return (
             <TodoItems
